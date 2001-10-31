aea9f2fdebb7daf2c0b16af9430bb05a07ab86b1
tjg <tjg>
2001-10-31 12:30:00 +0000

    Feature: large file support (thanks Scott Schwartz, Chris   Siebenmann).

--- tripping.c
+++ tripping.c
@@ -1,7 +1,12 @@
 /* This is an auxiliary test program for rc.  */
 
+#include "config.h"
+
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 static void out0(void) {
 	putchar('t'); putchar('r');
@@ -12,6 +17,21 @@ static void out0(void) {
 
 static void ctrl_a(void) {
 	puts("a\001ab\002b");
+}
+
+static void large(void) {
+	char c = 'c';
+
+#define LARGE (((off_t)1<<33)+1)
+
+	if (lseek(1, LARGE, SEEK_SET) != LARGE) {
+		perror("lseek");
+		exit(1);
+	}
+	if (write(1, &c, 1) == -1) {
+		perror("write");
+		exit(1);
+	}
 }
 
 static void makenonblock(void) {
@@ -31,6 +51,9 @@ int main(int argc, char **argv) {
 		break;
 	case 'a':
 		ctrl_a();
+		break;
+	case 'l':
+		large();
 		break;
 	case 'n':
 		makenonblock();
