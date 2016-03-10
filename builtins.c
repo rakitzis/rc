736d9da88a223adca795e959caacb3fe5d293f56
borkovic <borkovic@nomail.com>
2016-03-10 15:39:32 -0800

    order builtin declarations alphabetically

--- builtins.c
+++ builtins.c
@@ -21,9 +21,9 @@
 #include "rlimit.h"
 #include "sigmsgs.h"
 
-static void b_break(char **), b_cd(char **), b_eval(char **), b_exit(char **),
-	b_newpgrp(char **), b_return(char **), b_shift(char **), b_umask(char **),
-	b_wait(char **), b_whatis(char **), b_continue(char **);
+static void b_break(char **), b_cd(char **), b_continue(char **), b_eval(char **),
+        b_exit(char **), b_newpgrp(char **), b_return(char **), b_shift(char **),
+        b_umask(char **), b_wait(char **), b_whatis(char **);
 
 #if HAVE_SETRLIMIT
 static void b_limit(char **);
