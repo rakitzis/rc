86d6a8781c9852f867210049fa217815d4a10e32
Drazen Borkovic <borkovic@users.noreply.github.com>
2016-03-10 21:20:23 -0800

    Put declaration of b_continue() inside '#if RC_ADDON'

--- builtins.c
+++ builtins.c
@@ -21,9 +21,9 @@
 #include "rlimit.h"
 #include "sigmsgs.h"
 
-static void b_break(char **), b_cd(char **), b_continue(char **), b_eval(char **),
-        b_exit(char **), b_newpgrp(char **), b_return(char **), b_shift(char **),
-        b_umask(char **), b_wait(char **), b_whatis(char **);
+static void b_break(char **), b_cd(char **), b_eval(char **), b_exit(char **),
+        b_newpgrp(char **), b_return(char **), b_shift(char **), b_umask(char **),
+        b_wait(char **), b_whatis(char **);
 
 #if HAVE_SETRLIMIT
 static void b_limit(char **);
@@ -31,6 +31,10 @@ static void b_limit(char **);
 
 #if RC_ECHO
 static void b_echo(char **);
+#endif
+
+#ifdef RC_ADDON
+static void b_continue(char **);
 #endif
 
 static struct {
