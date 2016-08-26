d1623163519d9d58d68526d4a7e35c3388a27b15
borkovic <borkovic@nomail.com>
2016-08-26 12:00:08 -0700

    Add program to read one line from the input - substitute for bash:read

---
+++ Doc/line.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+int main (int ac, char* av[])
+{
+  int l;
+  char buf[1024];
+  const char* p = fgets(buf, sizeof(buf)-1, stdin);
+  if (! p) {
+    return 1;
+  }
+
+  l = strlen(buf);
+  fprintf(stderr, "L=%d, b=<%s>\n", l, buf);
+
+  if (l <= 1) {  // empty or newline alone
+    return 1;
+  } else {
+    if (buf[l-1] == '\n') {
+      buf[l-1] = '\0';
+      --l;
+    }
+    write(1, buf, l);
+    return 0;
+  }
+  return 1;
+}
+
