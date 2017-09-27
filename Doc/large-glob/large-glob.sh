3e2355d67db2a049d5be00e93d97919cab8b05fe
Drazen Borkovic <borkovic@nomail>
2017-09-26 20:14:19 -0500

    More error checking to large-glob scripts
    Change-Id: Iaff1ac7ee6bf9d914ca95432e59af255b0daec9b

---
+++ Doc/large-glob/large-glob.sh
@@ -0,0 +1,35 @@
+Error () { echo 1>&2 "$@"; }
+Fatal () { Error "$@"; exit 1; }
+
+###################################################################################################################
+case $# in 
+(1) : ;;
+(*) Fatal Usage: $0 '<POSITIVE_NUMBER>' ;;
+esac
+
+if ! let "N=$1"; then
+    Fatal Usage: $0 '<POSITIVE_NUMBER>'
+fi
+if let "$N <= 0"; then
+    Fatal Usage: $0 '<POSITIVE_NUMBER>'
+fi
+
+###################################################################################################################
+str='aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+
+i=0
+pat=''
+
+###################################################################################################################
+while let "$i < $N"
+do
+    pat=$pat'a*'
+    let "i = $i+1"
+done
+pat=$pat'b'
+
+###################################################################################################################
+cmd='case '$str' in ('$pat') echo case-yes;; (*) echo case-no;; esac'
+#echo $cmd; echo
+eval $cmd
+
