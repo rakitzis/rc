75c20ae6aa1e5f9c75ca515e4cbcb009fa12a3b1
Borkovic <borkovic@nomail>
2017-10-17 16:43:08 -0700

    Replace 'local' with 'typeset' for ksh

--- drazen/glob-exp/glob-exp.sh
+++ drazen/glob-exp/glob-exp.sh
@@ -3,7 +3,7 @@
 Script=$0
 Error () { echo 1>&2 "$@"; }
 Fatal () { Error "$@"; exit 1; }
-Usage () { Fatal Usage: $Script '[-if|-switch|-file|-echo]' '<POSITIVE_NUMBER>'; }
+Usage () { Fatal Usage: $Script '[-switch|-file|-echo]' '<POSITIVE_NUMBER>'; }
 
 DO_CASE=false
 DO_FILE=false
@@ -12,22 +12,18 @@ ECHO_CMD=false
 while let "$# > 0"
 do
     case x$1 in
-    (x-case|x--case)
-        DO_CASE=true ;;
-    (x-file|x--file|x-ls|x--ls)
-        DO_FILE=true ;;
-    (x-echo|x--echo)
-        ECHO_CMD=true ;;
-    (*)
-        break ;;
+    (x-case|x--case|x-switch|x--switch) DO_CASE=true ;;
+    (x-file|x--file|x-ls|x--ls)         DO_FILE=true ;;
+    (x-echo|x--echo)                    ECHO_CMD=true ;;
+    (*)                                 break ;;
     esac
     shift
 done
 
 ###########################################################################
-let "$# == 1" || Fatal Usage: $0 '<POSITIVE_NUMBER>' 
-let "N=$1"    || Fatal Usage: $0 '<POSITIVE_NUMBER>'
-let "$N <= 0" && Fatal Usage: $0 '<POSITIVE_NUMBER>'
+let "$# == 1" || Usage
+let "N=$1"    || Usage
+let "$N <= 0" && Usage
 
 ###########################################################################
 str='aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
@@ -45,7 +41,7 @@ echo 'shell=sh, N='$N
 
 
 EvalCmd () { 
-    local cmds="$@"
+    typeset cmds="$@"
     if $ECHO_CMD; then
         echo "Cmd: $cmds"
         echo -n 'Result: '
