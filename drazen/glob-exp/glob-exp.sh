1cc5651936df8983704a9f8c93ef269f49c7bc30
Borkovic <borkovic@nomail>
2017-10-19 12:58:30 -0700

    Added --all option to both glob-exp scripts

--- drazen/glob-exp/glob-exp.sh
+++ drazen/glob-exp/glob-exp.sh
@@ -3,7 +3,7 @@
 Script=$0
 Error () { echo 1>&2 "$@"; }
 Fatal () { Error "$@"; exit 1; }
-Usage () { Fatal Usage: $Script '[-switch|-file|-echo]' '<POSITIVE_NUMBER>'; }
+Usage () { Fatal Usage: $Script '[--case|--file|--all|--echo]' '<POSITIVE_NUMBER>'; }
 
 DO_CASE=false
 DO_FILE=false
@@ -14,6 +14,7 @@ do
     case x$1 in
     (x-case|x--case|x-switch|x--switch) DO_CASE=true ;;
     (x-file|x--file|x-ls|x--ls)         DO_FILE=true ;;
+    (x-all|x--all)                      DO_CASE=true; DO_FILE=true ;;
     (x-echo|x--echo)                    ECHO_CMD=true ;;
     (*)                                 break ;;
     esac
@@ -52,7 +53,7 @@ EvalCmd () {
 ###########################################################################
 ###########################################################################
 if $DO_CASE; then
-    cmd='case '$str' in ('$pat') echo case-yes;; (*) echo case-no;; esac'
+    cmd='case '$str' in ('$pat') echo case: yes match;; (*) echo case: no match;; esac'
     EvalCmd $cmd
 fi
 
